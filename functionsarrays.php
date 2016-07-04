804a1f6b2ede6dccb8b706872953e84429c9ce5a
Raul Villarreal <rvillarrcal@cnm.edu>
2016-07-04 11:25:17 -0600

    created .gitignore created functionsarrays.php

---
+++ functionsarrays.php
@@ -0,0 +1,19 @@
+<html>
+	<p>
+		<?php
+		// Round a floating point number to an integer
+		// and print it to the screen
+		$redondo = round(9.59483);
+		print $redondo;
+
+		?>
+	</p>
+	<p>
+		<?php
+		// Round a floating point number to 3 decimal places
+		// and print it to the screen
+		$redondo3 = round(2.9049494, 3);
+		print $redondo3;
+		?>
+	</p>
+</html>
